2743440fd16dccbbbc350c644673e964bc7ad8d1
mamun <mamuninfo@gmail.com>
2016-02-25 17:51:53 +0100

    Update

---
+++ example/cljs-app/src/app/ui.cljs
@@ -0,0 +1,24 @@
+(ns app.ui
+  (:require [sablono.core :as html :refer-macros [html]]
+            [json-html.core :as jhtml]))
+
+
+(defn table [[header & row]]
+  (html
+    [:table.mdl-data-table.mdl-js-data-table.mdl-data-table--selectable.mdl-shadow--2dp
+     [:thead
+      [:tr
+       (for [h header]
+         [:th.full-width {:key h} (str h)])]]
+     [:tbody
+      (for [r row]
+        [:tr {:key r}
+         (for [c r]
+           [:td {:key c} (str c)])])]]
+    ))
+
+
+
+
+(defn edn [data]
+  (html (jhtml/edn->hiccup data)))
